4587f5679309f116838affcfb714a9ea28c9639c
Anton Bäckström <antonbcm@gmail.com>
2017-06-21 11:38:53 +0200

    Torches now allow you to expand one tile more

--- src/TechTree.ts
+++ src/TechTree.ts
@@ -1,4 +1,5 @@
 import { TechNode } from "./TechNode";
+import { Square } from "./Square";
 import { Mine, Quary } from "./buildings/AllBuildings";
 // import { Quary } from "./Quary";
 import { ToolWorkshop, Kiln, Steelworks } from "./constructions/AllConstructions";
@@ -18,9 +19,13 @@ function enableKiln(){
 function enableSteelworks(){
   Steelworks.enabled = true;
 }
+function enableTorch(){
+  Square.buildDistance++;
+}
 let quaring = new TechNode("Quaring", [], 15, enableQuary);
 let mining = new TechNode("Mining", [quaring], 50, enableMine);
 let tooling = new TechNode("Tooling", [quaring], 50, enableToolWorkshop);
 let burning = new TechNode("Burning", [quaring], 50, enableKiln);
 let steelmaking = new TechNode("Steelmaking", [mining, burning], 100, enableSteelworks);
-export let TechList = [quaring, mining, tooling, burning, steelmaking];
+let torch = new TechNode("Torch", [burning], 100, enableTorch);
+export let TechList = [quaring, mining, tooling, burning, steelmaking, torch];
