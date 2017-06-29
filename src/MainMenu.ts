aaeccc0891453f5cdd8597f75675e1ef07b484bf
Anton Bäckström <antonbcm@gmail.com>
2017-06-29 14:41:00 +0200

    Updated and added sprite-sheets

--- src/MainMenu.ts
+++ src/MainMenu.ts
@@ -13,6 +13,7 @@ export class MainMenu extends Phaser.State {
     this.game.load.atlasJSONHash('resources', 'assets/resources.png', 'assets/resources.json');
     this.game.load.atlasJSONHash('buildings', 'assets/buildings.png', 'assets/buildings.json');
     this.game.load.atlasJSONHash('menu', 'assets/menu.png', 'assets/menu.json');
+    this.game.load.atlasJSONHash('tutorial', 'assets/tutorial.png', 'assets/tutorial.json');
   }
 
   create() {
